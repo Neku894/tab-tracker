edd3071ee6881d8298d88bd0ddeb47cfbb6931a5
Denis-Denis <greyhotel2@gmail.com>
2018-06-26 16:49:13 +0300

    on lesson 3

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email">
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password">
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2 custom">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            v-model="password"
+          ></v-text-field>
+          <br>
+          <div class="error" v-html="error"></div>
+          <br>
+          <v-btn dark class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -24,25 +32,30 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password,
-        test: function ()
-        {
-          console.log(this.email)
-          console.log(this.password)
-        }
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error {
+  color: red;
+}
+.custom {
+  margin-top:150px;
+}
 </style>
